581168798912566d96b108bbe2ceb3ede8fc99f3
arturbrilliantov222 <70976041+arturbrilliantov222@users.noreply.github.com>
2023-09-25 16:05:18 +0300

    Comments added

--- src/main/java/Homeworks/Homework4.java
+++ src/main/java/Homeworks/Homework4.java
@@ -2,10 +2,10 @@ package Homeworks;
 
 public class Homework4 {
     public static void main(String[] args) {
-        int ageHuman;
-        int catsAge;
-        ageHuman = 22;
-        catsAge = ageHuman * 7;
-        System.out.println(ageHuman + " human years equals " + catsAge + " for cat's in human years");
+        int ageHuman; // Inserts variable of human age
+        int catsAge;   // Inserts variable of cats age
+        ageHuman = 22; // Getting value to its variable
+        catsAge = ageHuman * 7; // Counts cats years in human
+        System.out.println(ageHuman + " human years equals " + catsAge + " for cat's in human years");  // Prints out text
     }
 }
